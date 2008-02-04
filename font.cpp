c23c37f34f88d0baae0db493eb6791a8f92a7ad0
lauri <devnull@localhost>
2008-02-04 19:10:06 +0200

    [svn] Wrapped font to a struct

--- font.cpp
+++ font.cpp
@@ -1,10 +1,11 @@
-/* -*-c++-*-
- *
- * font.cpp
- *
- * Created on 23.1.2008, 18:27:28
- *
- */
+/* -*- c++ -*-
+
+font.cpp
+
+Created: Mon Feb  4 18:19:55 2008 lauri
+Last modified: Mon Feb  4 18:19:55 2008 lauri
+
+*/
 #include "display.h"
 #include "font.h"
 #ifndef OS_UNIX
@@ -23,16 +24,16 @@
 
 #endif
 
-
-
 VALUE font_initialize_impl(VALUE str, VALUE size, VALUE self)
 {
     rb_iv_set(self,"@path", str);
     rb_iv_set(self,"@size", size);
     char *path = RSTRING(str)->ptr;
+    printf("got font %s\n",path);
     FTGLTextureFont* font = new FTGLTextureFont(path);
     font->FaceSize(INT2NUM(size));
-    return INT2FIX((unsigned int) font);
+    Data_Wrap_Struct(self, 0 ,free, font);
+    return self;
 }
 
 /*
@@ -40,10 +41,13 @@ VALUE font_initialize_impl(VALUE str, VALUE size, VALUE self)
  *
  */
 
-VALUE font_render_impl(VALUE font_num, VALUE text, VALUE self)
+VALUE font_render_impl(VALUE text, VALUE self)
 {
     char *str = RSTRING(text)->ptr;
-    unsigned int font = NUM2UINT(font_num);
+
+    
+    void *font;
+    Data_Get_Struct(self, FTGLTextureFont, font);
     glPushMatrix();
     glEnable(GL_TEXTURE_2D);
     glColor3f(1.0, 1.0, 1.0);
@@ -61,6 +65,6 @@ void init_font()
   rb_font = rb_define_class("Font", rb_cObject);
   rb_define_singleton_method(rb_font, "new",
 			     (ruby_method*) &font_initialize_impl, 2);
-  rb_define_method(rb_font,"render",(ruby_method*) &font_render_impl, 2);
-  
+  rb_define_method(rb_font,"render",(ruby_method*) &font_render_impl, 1);
+
 }
