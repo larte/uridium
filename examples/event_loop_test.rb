bbcf4195bc758240ee6dd90ab6390bfe0f70c0fa
niko <devnull@localhost>
2008-02-17 17:29:07 +0200

    [svn] - Made the logo movement FPS independent.

--- examples/event_loop_test.rb
+++ examples/event_loop_test.rb
@@ -6,33 +6,50 @@ require 'event_loop'
 
 # Open a display.
 Uridium.init
-font = Font.new("fonts/goodtimes.ttf", 100)
+font = Font.new("fonts/goodtimes.ttf", 60)
 display = Display.new("Event Loop", 640, 480, false, true)
 display.open()
 gdi = display.gdi
 
-# Initial color.
-text = 'Uridium 000'
-frames = 0
+# Create a simple actor.
+class Logo
+  attr_accessor :text, :rotation, :scale
+  
+  def initialize
+    @text = ''
+    @rotation = 0
+    @scale = 1.0
+  end
+end
 
-# Simulation (uses Ruby 1.9's new lambda syntax).
+logo = Logo.new
+
+# Simulation.
 sim = lambda {|t, dt|
-  # Update color with random value.
-  text.succ!
-  return (frames += 1) < 100
+  # Update logo attributes.
+  logo.text = "Uridium (t: #{sprintf("%1.1f", t)}s, dt: #{sprintf("%1.2f", dt)}s)"
+  logo.rotation += dt * 2
+  logo.scale += dt * 0.1
+  return t < 10
 }
 
 # Renderer.
 renderer = lambda {|sim|
+  # Clear and setup identity transform.
   gdi.clear
-  gdi.rotate_z(0.1)
-  gdi.scale(1.002)
-  gdi.draw_text(font, text)
+  gdi.trans0
+  
+  # Apply logo transformation.
+  gdi.rotate_z(logo.rotation)
+  gdi.scale(logo.scale)
+
+  # Draw logo.
+  gdi.draw_text(font, logo.text)
   gdi.flip
 }
 
-# Create an event loop with sim dt=0.1s
-# (the "simulation" is updated once every 1/10th second)
+# Create an event loop with sim dt=0.05s
+# (the "simulation" is updated once every 1/20th second)
 EventLoop.new(0.05, sim, renderer).run
 
 # Clean up
