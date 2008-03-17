15a7e632ca779b4cdfa0194c3dca40f702e4ba78
lauri <devnull@localhost>
2008-03-17 16:42:00 +0200

    [svn] 	HOT TIP: 	1. remember that "sleep" stands for something on *NIX 	2. Code moar 'stredoids 	3. ??? 	4. PROFIT

--- ext/clock.cpp
+++ ext/clock.cpp
@@ -15,17 +15,15 @@ extern "C"
 
 static VALUE rb_clock;
 
-VALUE ticks()
+VALUE ticks(VALUE self)
 {
   return INT2FIX(SDL_GetTicks());
 }
 
-VALUE sleep(VALUE time)
+VALUE sleep_impl(VALUE self, VALUE time)
 {
-      printf("sleep %d\n",FIX2UINT(time));
       SDL_Delay(FIX2UINT(time));
-
-  return Qnil;
+      return Qnil;
 }
 
 void init_clock()
@@ -33,7 +31,7 @@ void init_clock()
   rb_clock = rb_define_class("Clock", rb_cObject);
 	
   rb_define_singleton_method(rb_clock, "ticks", (ruby_method*) &ticks, 0);
-  rb_define_singleton_method(rb_clock, "sleep", (ruby_method*) &sleep, 1);
+  rb_define_singleton_method(rb_clock, "sleep", (ruby_method*) &sleep_impl, 1);
 }
 
 
