b8e0792c50880145d9b4fdba2e54a171263ef562
larte <lauri.arte@gmail.com>
2018-02-17 20:33:21 +0200

    Edit extconf.rb for headers and libs depending on platform

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,21 +1,47 @@
 require 'mkmf'
 
+
 COMMON_LIBS = %w[SDL2 SDL2_mixer freetype ftgl].freeze
 NIX_LIBS = %w[GLU GL].freeze
+DARWIN_LIBS = %w[]
 
 HEADERS = { 'SDL2/SDL_mixer.h' => ['/usr', '/usr/local'],
             'ft2build.h' =>
            ['/usr/include/freetype2', '/usr/local/include/freetype2'] }.freeze
 
+NIX_HEADERS = {
+  'GL/gl.h' => ['/usr', '/usr/local'],
+  'GL/glu.h' => ['/usr', '/usr/local'],
+  'GL/glext.h' => ['/usr', '/usr/local']
+}
+DARWIN_HEADERS = {
+  'OpenGL/gl.h' => ['/usr', '/usr/local'],
+  'OpenGL/glu.h' => ['/usr', '/usr/local'],
+  'OpenGL/glext.h' => ['/usr', '/usr/local']
+}
+
+if RUBY_PLATFORM =~ /darwin/
+  # openGL
+  $CPPFLAGS += ' -DOS_DARWIN'
+  libs = COMMON_LIBS + DARWIN_LIBS
+  headers = HEADERS.merge DARWIN_HEADERS
+else
+  libs = COMMON_LIBS + NIX_LIBS
+  headers = HEADERS.merge NIX_HEADERS
+end
+
+
+
+
 # Check libs
 missing = []
-(COMMON_LIBS + NIX_LIBS).each do |l|
+libs.each do |l|
   missing << l unless have_library(l)
 end
 
 abort "can't find #{missing.join(", ")} libs" unless missing.empty?
 
-HEADERS.each do |header, paths|
+headers.each do |header, paths|
   find_header(header, *paths)
 end
 
@@ -31,9 +57,5 @@ cpp_unkown_warnflags.each do |f|
   CONFIG['warnflags'].slice!(/ #{f}/)
 end
 
-if RUBY_PLATFORM =~ /darwin/
-  # openGL
-  $CPPFLAGS += ' -DOS_DARWIN'
-end
 
 create_makefile('uridium')
